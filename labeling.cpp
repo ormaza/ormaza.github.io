9f1e6178850550f8f60f8fe26d41329dc0e224d6
ORMAZA <ormazaball@yahoo.com.br>
2016-03-18 14:08:26 -0300

    pdi commit

--- labeling.cpp
+++ labeling.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <math.h>
 
 using namespace cv;
 
---
+++ motiondetector.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char** argv){
+  Mat image;
+  int width, height;
+  VideoCapture cap;
+  vector<Mat> planes;
+  Mat histR, histG, histB,histOldR;
+  int nbins = 64;
+  float range[] = {0, 256};
+  const float *histrange = { range };
+  bool uniform = true;
+  bool acummulate = false;
+  double percent=0;
+  int times=0;
+
+  cap.open(0);
+  
+  if(!cap.isOpened()){
+    cout << "cameras indisponiveis";
+    return -1;
+  }
+  
+  width  = cap.get(CV_CAP_PROP_FRAME_WIDTH);
+  height = cap.get(CV_CAP_PROP_FRAME_HEIGHT);
+
+  cout << "largura = " << width << endl;
+  cout << "altura  = " << height << endl;
+
+  int histw = nbins, histh = nbins/2;
+  Mat histImgR(histh, histw, CV_8UC3, Scalar(0,0,0));
+  Mat histImgG(histh, histw, CV_8UC3, Scalar(0,0,0));
+  Mat histImgB(histh, histw, CV_8UC3, Scalar(0,0,0));
+
+  //calculo do histograma histOldR
+  cap >> image;
+  split (image, planes);
+  calcHist(&planes[0], 1, 0, Mat(), histOldR, 1,
+             &nbins, &histrange,
+             uniform, acummulate);
+    normalize(histOldR, histOldR, 0, 1, NORM_MINMAX, -1, Mat());
+
+  while(1){
+    cap >> image;
+    split (image, planes);
+    calcHist(&planes[0], 1, 0, Mat(), histR, 1,
+             &nbins, &histrange,
+             uniform, acummulate);
+    calcHist(&planes[1], 1, 0, Mat(), histG, 1,
+             &nbins, &histrange,
+             uniform, acummulate);
+    calcHist(&planes[2], 1, 0, Mat(), histB, 1,
+             &nbins, &histrange,
+             uniform, acummulate);
+
+    normalize(histR, histR, 0, histImgR.rows, NORM_MINMAX, -1, Mat());
+    normalize(histG, histB, 0, histImgR.rows, NORM_MINMAX, -1, Mat());
+    normalize(histB, histB, 0, histImgR.rows, NORM_MINMAX, -1, Mat());
+
+    histImgR.setTo(Scalar(0));
+    histImgG.setTo(Scalar(0));
+    histImgB.setTo(Scalar(0));
+    
+    for(int i=0; i<nbins; i++){
+      line(histImgR, Point(i, histh),
+           Point(i, cvRound(histR.at<float>(i))),
+           Scalar(0, 0, 255), 1, 8, 0);
+      line(histImgG, Point(i, histh),
+           Point(i, cvRound(histG.at<float>(i))),
+           Scalar(0, 255, 0), 1, 8, 0);
+      line(histImgB, Point(i, histh),
+           Point(i, cvRound(histB.at<float>(i))),
+           Scalar(255, 0, 0), 1, 8, 0);
+    }
+    
+    if(times<3) percent = compareHist(histR,histOldR,CV_COMP_CORREL);
+
+    if(percent<0.95)
+    {
+        putText( image, "ALARME!", Point(height/2,width/2), CV_FONT_HERSHEY_COMPLEX, 2,
+           Scalar(0, 0, 255), 5, 0 );
+        times++;
+    }
+    histOldR = histR.clone();
+    cout<<percent<<endl;
+    imshow("image", image);
+    if(waitKey(30) >= 0) break;
+  }
+  return 0;
+}
